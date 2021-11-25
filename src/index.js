7aaa11f0e3009a34cb2936a2e1a508ca5454742c
정현수 <54893898+junghyeonsu@users.noreply.github.com>
2021-11-25 20:54:10 +0900

    refactor: class를 function으로 변경

--- src/index.js
+++ src/index.js
@@ -1,20 +1,18 @@
 import { $, pickUniqueNumbers } from './utils/index.js';
 import { START_NUMBER_RANGE, END_NUMBER_RANGE, COUNT_OF_NUMBERS } from '../constants.js';
 
-export default class BaseballGame {
-  constructor () {
-    this.answer = pickUniqueNumbers(START_NUMBER_RANGE, END_NUMBER_RANGE, COUNT_OF_NUMBERS);
+function BaseballGame () {
+  this.answer = pickUniqueNumbers(START_NUMBER_RANGE, END_NUMBER_RANGE, COUNT_OF_NUMBERS);
+
+  this.init = () => {
+    initEventListeners();
   }
 
-  init () {
-    this.initEventListeners();
+  const initEventListeners = () => {
+    $('form').addEventListener('submit', onSubmitPlayerInput);
   }
 
-  initEventListeners () {
-    $('form').addEventListener('submit', this.onSubmitPlayerInput);
-  }
-
-  onSubmitPlayerInput (event) {
+  const onSubmitPlayerInput = (event) => {
     event.preventDefault();
     const input = $('#user-input').value;
     console.log(`제출된 값: ${input}`);
